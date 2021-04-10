c5c77dd28d48c22f78db4a99852acd3b09ee05ab
Devananth Nkl <52454081+Devananth@users.noreply.github.com>
2021-04-10 10:53:43 +0530

    Mark-six-Done

---
+++ app.js
@@ -0,0 +1,34 @@
+var btnTranslate = document.querySelector("#translate-btn");
+
+var txtInput = document.querySelector("#translate-ip");
+
+var outputDiv = document.querySelector("#translate-op");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function constructUrl(input)
+{
+    return serverUrl+"?text="+input;
+}
+
+function errorHandler(error)
+{
+    console.log("error occured ",error);
+    alert("Something went wrong in server");
+}
+
+function clickHandler()
+{
+    console.log("Button Clicked");
+    var input = txtInput.value;
+    var finalUrl = constructUrl(input);
+    console.log(finalUrl);
+    fetch(finalUrl)
+        .then(response => response.json())
+        .then(json => {
+            outputDiv.innerText = json.contents.translated;
+        })
+        .catch(erroHandler);
+        
+}
+btnTranslate.addEventListener("click" , clickHandler);
